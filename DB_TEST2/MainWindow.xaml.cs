62f8b74a632305cc2d2cfa2a350083e39ccc7274
cristhian ganchala <cis.ganchala@yavirac.edu.ec>
2020-07-20 15:14:32 -0500

    version1

---
+++ DB_TEST2/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace DB_TEST2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PostgresConection postgresC = new PostgresConection();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)                    //transaccion sin error
+        {
+            postgresC.transaccion(Convert.ToUInt64(txbx_Saldo1.Text), Convert.ToUInt64(txbx_Saldo2.Text), Convert.ToSingle(txbx_valor.Text));
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)                    //transaccion con error
+        {
+            postgresC.NO_transaccion(Convert.ToUInt64(txbx_Saldo1.Text), Convert.ToUInt64(txbx_Saldo2.Text), Convert.ToSingle(txbx_valor.Text));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)                   /// ATUALIZA datos en pantall 
+        {
+           try
+            {
+                postgresC.Desconectar();
+                postgresC.Conectar();
+                
+                Label_saldo1.Content = postgresC.ConsultaSaldo(Convert.ToUInt64(txbx_Saldo1.Text));
+                Label_saldo2.Content = postgresC.ConsultaSaldo(Convert.ToUInt64(txbx_Saldo2.Text));
+                postgresC.Desconectar();
+
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show($"{x}");
+            }
+            
+        }
+
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DB_TEST2/PostgresConection.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Npgsql;
+
+namespace DB_TEST2
+{
+    class PostgresConection
+    {
+        public NpgsqlConnection sqlPostDB = new NpgsqlConnection("Username = postgres; Password = admin123; Host = localhost; Port = 5432; Database = Transacciones");
+        DataSet data = new DataSet();
+        public void Conectar()
+        {
+            try
+            {
+                sqlPostDB.Open();
+                MessageBox.Show("Conecting...");
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show($"{x}");
+            }
+        }
+        public void Desconectar()
+        {
+            try
+            {
+                sqlPostDB.Close();
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show($"{x}");
+            }
+        }
+ 
+        public string ConsultaSaldo(ulong cuenta)
+        {
+            string query = $"select saldo_cuenta from usuarios where numero_cuenta = '{cuenta}'";
+            NpgsqlCommand conector  = new NpgsqlCommand(query,sqlPostDB);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+            DataTable table = new DataTable();
+            datos.Fill(table);
+            string dato = table.Rows[0]["saldo_cuenta"].ToString();
+            return dato;
+
+        }
+
+        public void transaccion (ulong devitador, ulong cretidador, float valor)
+        {
+            this.Conectar();
+            string tra_op = $"BEGIN;";
+            string tra_dev = $"UPDATE usuarios SET saldo_cuenta = saldo_cuenta - {valor} Where numero_cuenta = { devitador}";
+            string tra_cre = $"UPDATE usuarios SET saldo_cuenta = saldo_cuenta + {valor} Where numero_cuenta = { cretidador}";
+            string tra_commit = $"COMMIT;";
+            string tra_rollback = $"ROLLBACK;";
+
+            try
+            {
+                NpgsqlCommand conector = new NpgsqlCommand(tra_op, sqlPostDB);
+                conector.ExecuteNonQuery();
+                NpgsqlCommand conector1 = new NpgsqlCommand(tra_dev, sqlPostDB);
+                conector1.ExecuteNonQuery();
+                NpgsqlCommand conector2 = new NpgsqlCommand(tra_cre, sqlPostDB);
+                conector2.ExecuteNonQuery();
+                NpgsqlCommand conector3 = new NpgsqlCommand(tra_commit, sqlPostDB);
+                conector3.ExecuteNonQuery();
+
+            }
+            catch (Exception x)
+            {
+                NpgsqlCommand conector4 = new NpgsqlCommand(tra_rollback, sqlPostDB);
+                conector4.ExecuteNonQuery();
+                MessageBox.Show($"{x}");
+            }
+
+            this.Desconectar();
+        }
+
+
+        public void NO_transaccion (ulong devitador, ulong cretidador, float valor)
+        {
+            this.Conectar();
+            string tra_op = $"BEGIN;";
+            string tra_dev = $"UPDATE usuarios SET saldo_cuenta = saldo_cuenta - {valor} Where numero_cuenta = { devitador}";
+            string tra_cre = $"UPDATE usuarios SET saldo_cuenta = saldo_cuenta + {valor} Where numero_cuenta = { cretidador}";
+            string tra_commit = $"COMMIT;";
+            string tra_rollback = $"ROLLBACK;";
+
+            try
+            {
+                NpgsqlCommand conector = new NpgsqlCommand(tra_op, sqlPostDB);
+                conector.ExecuteNonQuery();
+                NpgsqlCommand conector1 = new NpgsqlCommand(tra_dev, sqlPostDB);
+                conector1.ExecuteNonQuery();
+                sqlPostDB.Close();
+                NpgsqlCommand conector2 = new NpgsqlCommand(tra_cre, sqlPostDB);
+                conector2.ExecuteNonQuery();
+                NpgsqlCommand conector3 = new NpgsqlCommand(tra_commit, sqlPostDB);
+                conector3.ExecuteNonQuery();
+
+            }
+            catch (Exception x)
+            {
+                sqlPostDB.Open();
+                NpgsqlCommand conector4 = new NpgsqlCommand(tra_rollback, sqlPostDB);
+                conector4.ExecuteNonQuery();
+                MessageBox.Show($"{x}");
+            }
+
+
+
+            this.Desconectar();
+        }
+
+    }
+}
+
+
+
